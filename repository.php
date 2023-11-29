be4b28e1e2da20812a8bb757ff75ed076dd3c41a
Yulya Gaus <yulya.yulya.010202@gmail.com>
2023-11-29 16:00:39 +0300

    create data from db, change getTodos()

--- repository.php
+++ repository.php
@@ -2,19 +2,53 @@
 
 function getTodos(?int $time = null): array
 {
-	$filePath = getRepositoryPath($time);
+	$dbHost = '127.0.0.1';
+	$dbUser = 'root';
+	$dbPassword = '';
+	$dbName = 'todolist';
 
-	if(!file_exists($filePath))
+	$connection = mysqli_init();
+	$connected = mysqli_real_connect($connection, $dbHost, $dbUser, $dbPassword, $dbName);
+
+	if (!$connected)
 	{
-		return [];
+		$error = mysqli_connect_errno() . ': ' . mysqli_connect_error();
+		throw new Exception($error);
 	}
 
-	$content = file_get_contents($filePath);
-	$todos = unserialize($content, [
-		'allowed_classes' => false,
-	]);
+	$encodingResult = mysqli_set_charset($connection, 'utf8');
 
-	return is_array($todos) ? $todos : [];
+	if (!$encodingResult)
+	{
+		throw new Exception(mysqli_error($connection));
+	}
+
+	$from = date('Y-m-d 00:00:00', $time);
+	$to = date('Y-m-d 23:59:59', $time);
+
+	$result = mysqli_query($connection, "
+		SELECT * FROM todos
+		WHERE created_at BETWEEN '{$from}' AND '{$to}'
+		ORDER BY created_at
+		LIMIT 100
+	");
+	if (!$result)
+	{
+		throw new Exception(mysqli_error($connection));
+	}
+	$todos = [];
+	while ($row = mysqli_fetch_assoc($result))
+	{
+		$todos[] = [
+			'id' => $row['id'],
+			'title' => $row['title'],
+			'completed' => ($row['completed'] === 'Y'),
+			'created_at' => strtotime($row['created_at']),
+			'updated_at' => $row['updated_at'] ? strtotime($row['updated_at']) : null,
+			'completed_at' => $row['completed_at'] ? strtotime($row['completed_at']) : null,
+		];
+	}
+	return $todos;
 }
 
 function getRepositoryPath(?int $time): string
