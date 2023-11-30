27166f67fa363a2f13dc475bdec4601df73bec50
Yulya Gaus <yulya.yulya.010202@gmail.com>
2023-11-30 12:36:19 +0300

    create dir modules

--- boot.php
+++ boot.php
@@ -5,3 +5,4 @@ const ROOT = __DIR__;
 require_once ROOT . '/repository.php';
 require_once ROOT . '/services/index.php';
 require_once ROOT . '/components/index.php';
+require_once ROOT . '/models/index.php';
--- models/Todo.php
+++ models/Todo.php
@@ -1,35 +1,25 @@
 <?php
-function createToDo(string $title): array
-{
-	return [
-		'id' => uniqid('', true),
-		'title' => $title,
-		'completed' => false,
-		'created_at' => time(),
-		'updated_at' => null,
-		'completed_at' => null,
-	];
-}
-
 class Todo
 {
 	private string $id;
 
 	private string $title;
 
-	private bool $completed=false;
+	private bool $completed = false;
 
 	private int $createdAt;
 
-	private ?int $updatedAt=null;
+	private ?int $updatedAt = null;
 
-	private ?int $completedAt=null;
+	private ?int $completedAt = null;
+
 	public function __construct(string $title)
 	{
 		$this->id = uniqid('', true);
 		$this->setTitle($title);
 		$this->createdAt = time();
 	}
+
 	public function done()
 	{
 		$now = time();
@@ -37,16 +27,19 @@ class Todo
 		$this->completedAt = $now;
 		$this->updatedAt = $now;
 	}
+
 	public function undone()
 	{
 		$this->completed = false;
 		$this->completedAt = null;
 		$this->updatedAt = null;
 	}
+
 	public function isCompleted(): bool
 	{
 		return $this->completed;
 	}
+
 	public function getId(): string
 	{
 		return $this->id;
@@ -60,12 +53,12 @@ class Todo
 	public function setTitle(string $title): void
 	{
 		$title = trim($title);
-		if (strlen($title) === 0)
-		{
+		if (strlen($title) === 0) {
 			throw new Exception("Title cannon be empty");
 		}
 		$this->title = $title;
 	}
+
 	public function getCreatedAt(): int
 	{
 		return $this->createdAt;
@@ -81,10 +74,3 @@ class Todo
 		return $this->completedAt;
 	}
 }
-
-$todo = new Todo("hello world");
-
-$todo->done();
-var_dump('DONE!!!', $todo);
-$todo->undone();
-var_dump('UNDONE!!!', $todo);
